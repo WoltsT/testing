6872464e2ef2655643a2aa8eb0bb8333e5a00656
WoltsT <jeferson.montep@gmail.com>
2023-08-22 11:32:48 -0500

    TESTING

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -4,7 +4,7 @@ pipeline {
     environment {
         XAMPP_PATH = '/xampp'
         APACHE_BIN = "/xamppapache/bin"
-        HTDOCS_DIR = "/xampp/htdocs"
+        HTDOCS_DIR = "/xampp/htdocs/my-static-website"
     }
     
     stages {
@@ -17,7 +17,7 @@ pipeline {
         stage('Deploy to XAMPP') {
             steps {
                 script {
-                        sh "cp -r ./* ${HTDOCS_DIR}/"  // Copiar archivos al directorio htdocs
+                        sh "cp -r ./* ${HTDOCS_DIR}"  // Copiar archivos al directorio htdocs
                         sh "${APACHE_BIN}/apachectl restart"  // Reiniciar el servidor Apache
                     }
                 }
